906492f86dc68170ff72488297f9db7b79f7dab1
h146942 <h146942@stud.u-szeged.hu>
2025-10-18 13:40:16 +0200

    change seeder

--- backend/seeders/20251005162541-populate-product-variants.js
+++ backend/seeders/20251005162541-populate-product-variants.js
@@ -6,38 +6,72 @@ module.exports = {
     const FULL_SIZES = ["XS", "S", "M", "L", "XL", "2XL", "3XL", "4XL"];
 
     const rows = [
-      { name: "MonoSkull", color: ["black", "white"], size: FULL_SIZES },
+      {
+        name: "MonoSkull",
+        color: ["black", "white"],
+        size: FULL_SIZES,
+        gender: "Unisex",
+      },
       {
         name: "Muse Tee",
         color: ["black", "white"],
         size: ["S", "M", "L", "XL"],
+        gender: "Female",
       },
-      { name: "Secrets of the Lily", color: ["white"], size: ["M", "L"] },
-      { name: "Whispers of Grace", color: ["white"], size: ["S", "M"] },
-      { name: "Velvet Silence", color: ["black", "white"], size: FULL_SIZES },
+      {
+        name: "Secrets of the Lily",
+        color: ["white"],
+        size: ["M", "L"],
+        gender: "Female",
+      },
+      {
+        name: "Whispers of Grace",
+        color: ["white"],
+        size: ["S", "M"],
+        gender: "Female",
+      },
+      {
+        name: "Velvet Silence",
+        color: ["black", "white"],
+        size: FULL_SIZES,
+        gender: "Unisex",
+      },
       {
         name: "Shadows of Bloom",
         color: ["black", "white"],
         size: ["M", "L", "XL", "2XL"],
+        gender: "Female",
       },
       {
         name: "Ivory Bloom",
         color: ["black", "white"],
         size: ["L", "XL", "2XL", "3XL"],
+        gender: "Male",
       },
-      { name: "White Eclipse", color: ["black", "white"], size: FULL_SIZES },
-      { name: "Lily Veins", color: ["black"], size: ["S", "M", "L"] },
+      {
+        name: "White Eclipse",
+        color: ["black", "white"],
+        size: FULL_SIZES,
+        gender: "Unisex",
+      },
+      {
+        name: "Lily Veins",
+        color: ["black"],
+        size: ["S", "M", "L"],
+        gender: "Female",
+      },
       {
         name: "Monochrome Muse",
         color: ["black", "white"],
         size: ["M", "L", "XL"],
+        gender: "Female",
       },
     ];
 
     for (const r of rows) {
       await queryInterface.bulkUpdate(
         "Products",
-        { color: r.color, size: r.size, updatedAt: now },
+        { color: r.color, size: r.size, gender: r.gender, updatedAt: now },
         { name: r.name }
       );
     }
